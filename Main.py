86f379dcfea1b601f79669941377818a89aca369
amitaiyu2-art <amitaiyu2@gmail.com>
2026-02-06 16:36:10 -0800

    V1

---
+++ Main.py
@@ -0,0 +1,8 @@
+print("Hello!")
+input = input("I am Chat GPT. How are you feeling today? (good or bad) ").lower()
+if input == "good":
+    print("Glad to hear that!")
+elif input == "bad":
+    print("Im sorry to hear that! Hope you feel better soon!")
+else:
+    print("Its hard to explain your feelings sometimes.")
