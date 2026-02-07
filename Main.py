863e7ee28ffb696a2ea2f489f3b3b1e8a236d35b
amitaiyu2-art <amitaiyu2@gmail.com>
2026-02-06 17:05:11 -0800

    Homework for Friday Feb 6th, 2026

--- Main.py
+++ Main.py
@@ -1,8 +1,31 @@
 print("Hello!")
-input = input("I am Chat GPT. How are you feeling today? (good or bad) ").lower()
-if input == "good":
+name = input("What is your name? ")
+print(f"Hi {name}! I am Chat GPT. How are you feeling today? (good or bad) ")
+feelings = input("").lower()
+if feelings == "good":
     print("Glad to hear that!")
-elif input == "bad":
+elif feelings == "bad":
     print("Im sorry to hear that! Hope you feel better soon!")
 else:
     print("Its hard to explain your feelings sometimes.")
+print("Well, what is your favorite class?")
+favorite_class = input("").lower()
+print(f"Nice! I also think that {favorite_class} is an insanely fun subject!")
+print("Now, for the most important question...")
+cheese = input("Do you like cheese? This is a yes or no question any other question will result in nothing happening... ").lower()
+if cheese == "yes":
+    print("OK YOUR GOOD")
+elif cheese == "no":
+    print("noooo why do you not like cheese...")
+else:
+    print("This is a yes or no question. Try again! ")
+    while cheese != "yes" or cheese != "no":
+        cheese2 = input("do you like cheese").lower()
+        if cheese2 == "yes":
+            print("Finally... Good")
+            break
+        elif cheese2 == "no":
+            print("Dang it")
+            break
+        else:
+            print("Try again...")
